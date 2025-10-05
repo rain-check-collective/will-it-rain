5041dde9e6ad607ec2b32cc5dc6e44270513b164
Cynthia Black <cindy.black@gmail.com>
2025-10-05 13:12:32 -0800

    Working call to Mesonet API

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 Dataset used:
 - Historical FAA METAR via Iowa State Mesonet https://mesonet.agron.iastate.edu/api/
+- Retrieved: station, datetime (param: valid), ambient temperature (param: tmpf), relative humidity (param: relh), windspeed (param: sknt), precipitation (param: p01i)
 
 Tools used:
 - Requests (API calls)
--- backend/main.py
+++ backend/main.py
@@ -6,20 +6,29 @@ def main():
     ## vars for API requests   
     # Default Parameters to show data before user input        
     station = "KDCA"
-    date = "2024-12-31"
+    year = "2024"
+    month = "12"
+    day = "5"
 
-    # GET weather data via FAA METAR API  
-    metar_url = "https://aviationweather.gov/api/data/metar"
+    # GET weather data (historical FAA METAR observations) via Mesonet API  
+    metar_url = "https://mesonet.agron.iastate.edu/cgi-bin/request/asos.py"
     params = {
-        "ids": station,
-        "date": date,
-        "format": "json"
+        "station": station,
+        "data": "station,valid,tmpf,relh,sknt,p01i",
+        "year1": year,
+        "month1": month,
+        "day1": day,
+        "year2": year,
+        "month2": month,
+        "day2": day,
+        "tz": "UTC",
+        "format": "onlycomma"
     }
 
     print("Fetching METAR data ✈️") 
     res = requests.get(metar_url, params=params)
     res.raise_for_status()
-    metar_data = res.json()
+    metar_data = res.text
 
     print("METAR data:", metar_data)
 
