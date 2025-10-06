845ed19e2a33baa8305b838fb21564b577e782b3
Cynthia Black <cindy.black@gmail.com>
2025-10-05 21:51:41 -0800

    Troubleshooting CORS

--- backend/api.py
+++ backend/api.py
@@ -9,11 +9,12 @@ app = FastAPI()
 origins = [
     "http://localhost:5500",
     "http://127.0.0.1:5500",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
